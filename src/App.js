390d18ecde01c3015190a889acb03ffe58db2e56
tnhanbeehexa <thanh.nhan@beehexa.com>
2024-07-14 14:49:40 +0700

    video 16

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponent from "./components/MyComponent";
+
 const App = () => {
-  const count = useSelector(state => state.counter.count);
+  const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Xin chào mọi người
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div>
+        Xin chào thế giới
+        <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>Xin chào mọi người</p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
   );
-}
+};
 
 export default App;
