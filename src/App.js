0e3a8f3d167be1b2ebd16f62956706e7d5814ede
tnhanbeehexa <thanh.nhan@beehexa.com>
2024-07-14 15:10:57 +0700

    Video 17

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
+import React from "react";
 
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component {
+  
+    state =  {
+      name: 'Thành Nhân',
+      address: 'Hoi dan IT',
+      age: 21
+    };
+    //JSX
+    render() {
+      return (
+        <div>
+            Xin chào thế giới, tên của thôi là {this.state.name}
+            <MyComponent></MyComponent>
+        </div>
+      );
+    }
+}
 
-  return (
-    <div>
-        Xin chào thế giới
-        <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Xin chào mọi người</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-};
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Xin chào mọi người</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
+
 
 export default App;
